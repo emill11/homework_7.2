cd1da1c848fb3458f2212b850fb641b82297f115
Emil <emil88337@gmail.com>
2024-03-07 15:54:49 +0300

    remove file pack_in_zip.py

--- pack_in_zip.py
+++
@@ -1,8 +0,0 @@
-from zipfile import ZipFile
-
-with ZipFile("resources/zip.zip", 'w') as zip_file:
-    zip_file.write("resources/xlsx.xlsx")
-    zip_file.write("resources/pdf.pdf")
-    zip_file.write("resources/users.csv")
-
-    print(zip_file.namelist())
